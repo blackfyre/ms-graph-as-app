06d9affce6c375c668e8b38d09e4d404e6f86752
Miklós Galicz <galiczm@invitech.hu>
2021-04-13 14:23:27 +0200

    working prototype

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'azure_client_id' => env('AZURE_TENANT', ''),
+    'azure_client_id' => env('AZURE_APP_ID', ''),
     'azure_client_secret' => env('AZURE_APP_SECRET', ''),
     'azure_tenant_id' => env('AZURE_TENANT', 'common'),
-    'azure_client_scope' => env('AZURE_CLIENT_SCOPE', 'https://graph.microsoft.com/.default')
+    'azure_client_scope' => env('AZURE_APP_SCOPE', 'https://graph.microsoft.com/.default')
 ];
--- src/MsGraphAsApp.php
+++ src/MsGraphAsApp.php
@@ -2,9 +2,32 @@
 
 namespace Blackfyre\MsGraphAsApp;
 
+use Microsoft\Graph\Graph;
+
 class MsGraphAsApp
 {
-    private function getAuthToken() {
+    /**
+     * @var string
+     */
+    private $authToken;
+
+    /**
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getAuthToken()
+    {
+        if (!$this->authToken) {
+            $this->fetchAuthToken();
+        }
+        return $this->authToken;
+    }
+
+    /**
+     * Query M$ for an auth token
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    private function fetchAuthToken() {
         $guzzle = new \GuzzleHttp\Client();
         $url = 'https://login.microsoftonline.com/' . config('ms-graph-as-app.azure_tenant_id') . '/oauth2/v2.0/token';
         $payload = [
@@ -15,5 +38,22 @@ class MsGraphAsApp
                 'grant_type' => 'client_credentials',
             ],
         ];
+
+        $token = json_decode($guzzle->post($url, $payload)->getBody()->getContents());
+        $this->authToken = $token->access_token;
+
+    }
+
+    /**
+     * Get a configured GRAPH SDK instance, ready for any query
+     * @return Graph
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getGraph(): Graph
+    {
+        $graph = new Graph();
+        $graph->setAccessToken($this->getAuthToken());
+
+        return $graph;
     }
 }
